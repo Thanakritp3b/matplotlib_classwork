f83fd42b2228f44c07918c7e460532b1d233540e
Thanakrit Pongtanawannagon <thanakrit03b@gmail.com>
2025-01-23 10:07:46 +0700

    add color

--- matplotlib_cw.py
+++ matplotlib_cw.py
@@ -6,7 +6,7 @@ def f1(x):
     return x ** 2
 
 def f2(x):
-    return np.sin(x)
+    return x * np.sin(2 * x)
 
 def f3(x):
     return np.arctan(x)
@@ -16,8 +16,8 @@ y1 = f1(x)
 y2 = f2(x)
 y3 = f3(x)
 
-plt.plot(x, y1, label='x^2')
-plt.plot(x, y2, label='sin(x)')
-plt.plot(x, y3, label='arctan(x)')
+plt.plot(x, y1, label='x^2', color='red')
+plt.plot(x, y2, label='sin(x)', color='green')
+plt.plot(x, y3, label='arctan(x)', color='blue')
 plt.legend()
 plt.show()
